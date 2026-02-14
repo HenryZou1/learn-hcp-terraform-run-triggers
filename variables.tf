ccfd9f562bb08558e738a83ba2ff0a7a9889db70
HenryZou1 <39718373+HenryZou1@users.noreply.github.com>
2026-02-13 21:44:53 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "instance_name" {
   description = "Value of the EC2 instance's Name tag."
   type        = string
-  default     = "learn-hcp-terraform"
+  default     = "learn-hcp-terraform-run-triggers"
 }
 
 variable "instance_type" {
